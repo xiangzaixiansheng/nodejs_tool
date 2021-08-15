a88f92247e97aac302c049dadeab30092e91de56
hanxiang1 <hanxiang1@staff.sina.com.cn>
2021-08-15 21:28:46 +0800

    feat:add exectools

--- src/util/execTool.ts
+++ src/util/execTool.ts
@@ -1,5 +1,7 @@
 import { exec } from "child_process";
 import { promisify } from "util";
+import { spawn, spawnSync } from "child_process";
+
 const execify = promisify(exec);
 
 /**
@@ -10,5 +12,23 @@ const execify = promisify(exec);
 async function execPromise(data: string) {
     return await execify(data, {
         maxBuffer: 8000 * 1024
+    });
+}
+
+/**
+ * @description 指定命令，结束后带有返回值的那种 例如安卓手机录屏等
+ * @param command 
+ * @param args 
+ * @returns 
+ */
+export async function spawnWait(
+    command: string,
+    args: readonly string[]
+): Promise<number> {
+    return new Promise((resolve) => {
+        let proc = spawn(command, args, { stdio: "inherit" });
+        proc.on("close", function (code) {
+            return resolve(code || 0);
+        });
     });
 }
