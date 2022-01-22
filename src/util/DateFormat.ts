687cdeb710b96d5bff2f8c4a235f960c91a8ece3
hanxiang1 <hanxiang1@staff.sina.com.cn>
2022-01-22 11:43:43 +0800

    feat(date):增加获取时间范围的方法

--- src/util/DateFormat.ts
+++ src/util/DateFormat.ts
@@ -82,4 +82,89 @@ export class DateFormat {
         return today.subtract(days, "days").format("YYYY-MM-DD"); /*前一天的时间*/
     }
 
+    /**
+     * 根据时间刻度 day week month 生成对应的时间段列表
+     * step: 时间段刻度 day | week | month
+     * num: 输出多少组数据
+     * displayTimeFormat: 显示时间的格式(可自定义)
+     * rangeTimeFormat: 时间范围的格式(可自定义)
+    */
+    //getRangeTimeList("month", 12)
+    public getRangeTimeList(
+        step: "day" | "week" | "month",
+        num = 50,
+        displayTimeFormat = 'YYYY-MM-DD',
+        rangeTimeFormat = 'YYYY-MM-DD'
+    ) {
+        moment.locale('zh-cn')
+        let now = moment() // 当前日期
+        let result: any[] = []
+        const oneDayTime = 24 * 3600 // 一天的秒数 注意不是毫秒数
+        let currentUnix = now.unix() // 当前unix时间戳
+
+        const getTimeRange = (begin: any, end: any) => {
+            return [begin.format(rangeTimeFormat), end.format(rangeTimeFormat)]
+        }
+        const getDisplayTime = (begin: any, end: any) => {
+            return begin.format(displayTimeFormat) + '-' + end.format(displayTimeFormat)
+        }
+
+        const getYearMonth = (begin: any) => {
+            return begin.format('YYYY-MM');
+        }
+
+        if (step === 'day') {
+            for (let k = 1; k <= num; k++) {
+                const obj: any = {}
+                const day = moment.unix(currentUnix)
+                obj.timeRange = getTimeRange(day, day)
+                obj.tooltip = getDisplayTime(day, day)
+                result.push(obj)
+                currentUnix -= oneDayTime
+            }
+        }
+        if (step === 'week') {
+            // 处理当前这周
+            const lastWeek: any = {}
+            const firstDay = moment(now).weekday(0)
+            lastWeek.timeRange = getTimeRange(firstDay, now)
+            lastWeek.tooltip = getDisplayTime(firstDay, now)
+            currentUnix = firstDay.unix()
+            result.push(lastWeek)
+            // 处理剩余n-1周
+            for (let k = 2; k <= num; k++) {
+                const obj: any = {}
+                const sunday = moment.unix(currentUnix - oneDayTime) // 当前周-时间戳减去一天 等于上周日时间戳
+                const monday = moment(moment.unix(currentUnix - oneDayTime).weekday(0))
+                obj.timeRange = getTimeRange(monday, sunday)
+                obj.tooltip = getDisplayTime(monday, sunday)
+                result.push(obj)
+                currentUnix -= oneDayTime * 7
+            }
+        }
+        if (step === 'month') {
+            // 处理当前月
+            const lastMonth: any = {}
+            const firstDate = moment(now).date(1)
+            lastMonth.timeRange = getTimeRange(firstDate, now)
+            lastMonth.tooltip = getDisplayTime(firstDate, now)
+            lastMonth.monthKey = getYearMonth(firstDate);
+            currentUnix = firstDate.unix()
+            result.push(lastMonth)
+            // 处理剩余n-1个月
+            for (let k = 2; k <= num; k++) {
+                const obj: any = {}
+                const dayLast = moment.unix(currentUnix - oneDayTime) // 当前月第一天时间戳减去一天 等于上个月最后一天时间戳
+                const n = dayLast.date()
+                const day1 = moment(moment.unix(currentUnix - oneDayTime).date(1))
+                obj.timeRange = getTimeRange(day1, dayLast)
+                obj.tooltip = getDisplayTime(day1, dayLast)
+                obj.monthKey = getYearMonth(dayLast);
+                result.push(obj)
+                currentUnix -= oneDayTime * n
+            }
+        }
+        return result;
+    }
+
 }
