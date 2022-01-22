5c65fc3d41bea4a9ea8203f5243c959f81807ac6
hanxiang1 <hanxiang1@staff.sina.com.cn>
2022-01-22 11:46:38 +0800

    feat(redis):增加redis过期的key

--- src/glues/redis.ts
+++ src/glues/redis.ts
@@ -17,8 +17,26 @@ export function createRedisConnection() {
 
     return new Promise((resolve, reject) => {
         redis.on('connect', resolve);
+        //redis.on('connect', () => { RedisKeyExpire() });
         redis.on('error', reject);
     })
+}
+
+//监听redis过期的key
+function RedisKeyExpire() {
+    const db: string = redisConfig.db || "0";
+    redis.send_command('config', ['set', 'notify-keyspace-events', 'Ex'])
+        .then((res) => {
+            const expired_subKey = `__keyevent@${db}__:expired`;
+            const sub = new Redis(redisConfig);
+            sub.subscribe(expired_subKey, function () {
+                sub.on('message', function (info, expireKey) {
+                    console.info(`监听到过期的rediskey ${expireKey}`)
+                })
+            })
+        }).catch((err) => {
+            console.error("error", err);
+        })
 }
 
 
