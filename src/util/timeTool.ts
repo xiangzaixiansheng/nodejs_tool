370f2be333866cdba1f7e038c1eb7e25c9a0e5b1
hanxiang1 <hanxiang1@staff.sina.com.cn>
2022-02-25 11:30:47 +0800

    update

--- src/util/timeTool.ts
+++ src/util/timeTool.ts
@@ -2,7 +2,7 @@
  * @description 获取10位时间戳
  * @returns 
  */
- export const getCurTimeStamp = () => {
+export const getCurTimeStamp = () => {
     return Math.round(new Date().getTime() / 1000);
 }
 /**
@@ -11,4 +11,15 @@
  */
 export const getBeforeDaysTmp = (days: number = 2) => {
     return Math.round((new Date().getTime() - 24 * 60 * 60 * 1000 * days) / 1000);
+}
+
+
+export function formatTime(time: Date) {
+    const year = time.getFullYear();
+    const [month, day] = [time.getMonth() + 1, time.getDate()].map(formatNumber);
+    return `${year}-${month}-${day}`;
+}
+
+function formatNumber(n: number) {
+    return n < 10 ? `0${n}` : n;
 }
