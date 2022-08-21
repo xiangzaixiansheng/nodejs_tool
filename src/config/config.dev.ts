3b3b666549b5f2c5f0ddc208efd4e4a404ec7f7a
hanxiang1 <hanxiang1@staff.sina.com.cn>
2022-08-21 23:08:34 +0800

    feat(mysql):add mysql function

--- src/config/config.dev.ts
+++ src/config/config.dev.ts
@@ -7,5 +7,18 @@ export default {
     bullconfig: {
         queue1: "queue1",
         queue2: "queue2"
+    },
+    mysql: {
+        type: "mysql",
+        host: "localhost",
+        port: "3306",
+        username: "root",
+        password: "xiangzai",
+        database: "sqlstudy",
+        synchronize: process.env.NODE_ENV === 'dev',//是否进行数据库同步 线上环境必须为false
+        logging: process.env.NODE_ENV === 'dev',
+        timezone: "+8:00",
+        entities:
+            process.env.NODE_ENV === "dev" ? ["src/entities/*"] : ["build/entities/*"],
     }
 };
--- src/glues/index.ts
+++ src/glues/index.ts
@@ -1,10 +1,12 @@
+import { createMysqlConnection } from "./mysql";
 import { createRedisConnection } from "./redis";
 
 export default function createConnection() {
   return Promise.all([
     createRedisConnection().then(() => console.log("redis已连接")),
+    createMysqlConnection().then(() => console.log("mysql已连接"))
   ]).catch(err => {
-    console.error(`redis连接失败: ${err.message}`);
+    console.error(`连接失败: ${err.message}`);
     return Promise.reject(err);
   });
 }
